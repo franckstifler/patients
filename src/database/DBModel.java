b897a3c52d87ea7f10702372440a84e3041c586f
sahamene <saharmel100@gmail.com>
2016-11-27 11:40:29 +0100

    database Model

--- src/database/DBModel.java
+++ src/database/DBModel.java
@@ -1,5 +1,29 @@
 package database;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DBModel {
 
+    private PreparedStatement preparedStatement;
+
+    public void createDatabase() {
+        DBConnection connection = new DBConnection();
+        try {
+            preparedStatement = connection.getConnection().prepareStatement("create database if not exists Patient DEFAULT CHARACTER SET utf8 \n"
+                    + "  DEFAULT COLLATE utf8_general_ci");
+            preparedStatement.execute();
+            preparedStatement = connection.getConnection().prepareStatement("CREATE TABLE if not exists `Patient`.`patient` (\n"
+                    + "  `Id` int(11) NOT NULL AUTO_INCREMENT,\n"
+                    + "  `Nom` VARCHAR(20) NOT NULL,\n"
+                    + "  `Prenom` VARCHAR(100) ,\n"
+                    + "  `NumeroTel` VARCHAR(100) ,\n"
+                    + "  `AdressEmail` VARCHAR(100) ,\n"
+                    + "  `Address` text,\n"
+                    + "  PRIMARY KEY (`Id`));");
+            preparedStatement.execute();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
